fbdfd41eb2b5d95e65807fa2308a4bed313c7673
cherishMine <635862964@qq.com>
2018-08-03 14:49:50 +0800

    update

---
+++ src/components/Header.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="header">
+    <div class="content">
+      <div class="logo-wrapper">
+        <a href="/Home">
+          <img src="../assets/images/logo.png" alt="">
+        </a>
+      </div>
+      <ul class="header-nav">
+        <li>
+          <router-link to="/" tag="div">首页</router-link>
+        </li>
+        <li>
+          <router-link to="/" tag="div">交易中心</router-link>
+        </li>
+        <li>
+          <router-link to="/" tag="div">C2C交易</router-link>
+        </li>
+        <li>
+          <router-link to="/" tag="div">新闻资讯</router-link>
+        </li>
+        <li>
+          <router-link to="/" tag="div">个人中心</router-link>
+        </li>
+        <li>
+          <router-link to="/" tag="div">渠道商</router-link>
+        </li>
+        <li class="header-space"></li>
+        
+      </ul>
+      <ul class="nav-right-wrapper">
+        <li class="nav-right">注册</li>
+        <li class="split">|</li>
+        <li class="nav-right">登陆</li>
+        <li class="change-btn" @click="selectLanguage">
+          <i class="drop-icon" :class="{active:isActive}"></i>
+          <div class="language" >{{currentLanguage}}</div>
+          <transition name="fade">
+            <div class="dropdown" v-if="isShow">
+              <div @click="changeLanguage">中文</div>
+              <div @click="changeLanguage">English</div>
+            </div>
+          </transition>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VHeader",
+  data() {
+    return {
+      isShow:false,
+      currentLanguage:'中文',
+      isActive:false
+    };
+  },
+  methods:{
+    selectLanguage(){
+      this.isShow=!this.isShow
+      if(this.isShow){
+        this.isActive=true
+      }
+      else{
+        this.isActive=false
+      }
+    },
+    changeLanguage(e){
+      this.currentLanguage=e.target.innerText
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+.header {
+  height: 90px;
+  background: #1e212f;
+  .content {
+    margin: 0 120px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    .logo-wrapper {
+      a {
+        display: block;
+      }
+    }
+    .nav-right-wrapper {
+      display: flex;
+      align-items: center;
+      .nav-right,
+      .split {
+        opacity: 0.5;
+        font-family: PingFangTC-Regular;
+        font-size: 12px;
+        color: #ffffff;
+        margin-left: 0;
+      }
+      .split {
+        margin: 0 10px;
+      }
+      .change-btn {
+        width: 70px;
+        margin-left: 20px;
+        position: relative;
+        border: 1px solid #ffffff;
+          border-radius: 2px;
+          padding: 3px 0 3px 8px;
+          background: #1e2130;
+          opacity: 0.5;
+          color: #fff;
+          border-radius: 2px;
+          cursor: pointer;
+          div{
+             font-size: 12px;
+          }
+        .dropdown{
+          background: #1E2130;
+          position: absolute;
+          top: 25px;
+          left: 0;
+          width:80px;
+          padding: 4px 0 0 6px;
+          div{
+            line-height: 25px;
+          }
+          
+        }
+        .drop-icon {
+          width: 5px;
+          height: 5px;
+          display: inline-block;
+          border-left: 1px solid #ffffff;
+          border-top: 1px solid #ffffff;
+          transform: rotate(225deg);
+          -ms-transform: rotate(225deg);
+          -moz-transform: rotate(225deg);
+          -webkit-transform: rotate(225deg);
+          -o-transform: rotate(225deg);
+          opacity: 0.5;
+          position: absolute;
+          right: 9px;
+          top: 8px;
+          transition: ease-in-out 0.2s;
+        }
+        .drop-icon.active{
+          transform: rotate(45deg);
+        }
+      }
+    }
+    .header-nav {
+      display: flex;
+      align-items: center;
+      flex: 1 1 0%;
+      margin-left: 28px;
+      opacity: 0.8;
+
+      .header-space {
+        width: 242px;
+      }
+      li {
+        position: relative;
+        height: 90px;
+        line-height: 90px;
+        margin-left: 38px;
+        div {
+          font-family: PingFangTC-Medium;
+          font-size: 16px;
+          color: #fff;
+          letter-spacing: 0;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+}
+</style>
+
+
---
+++ src/pages/Home.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="hello">
+    <v-header></v-header>  
+    <swiper :options="swiperOption" ref="mySwiper">
+ <!-- slides -->
+ <swiper-slide>I'm Slide 1</swiper-slide>
+ <swiper-slide>I'm Slide 2</swiper-slide>
+ <swiper-slide>I'm Slide 3</swiper-slide>
+ <!-- Optional controls -->
+ <div class="swiper-pagination"  slot="pagination"></div>
+ <div class="swiper-button-prev" slot="button-prev"></div>
+ <div class="swiper-button-next" slot="button-next"></div>
+ <div class="swiper-scrollbar"   slot="scrollbar"></div>
+</swiper>
+
+    <div class="block-dark">
+      <img src="../assets/images/phone.png" alt="">
+      <div class="info download-info">
+        <dl>
+          <dt>IB.TOP移动端支持</dt>
+          <dd>覆盖iOS、Android多个移动端平台，支持全业务功能</dd>
+          <dd>集多种灵活交易模式于一体，体验一站式数字货币交易服务，币财富，一手掌握</dd>
+          <dd>
+            <div class="download-btn">安卓移动端下载</div>
+            <div class="download-btn">IOS移动端下载</div>
+          </dd>
+        </dl>
+      </div>
+    </div>   
+    <v-footer></v-footer>     
+  </div>
+</template>
+
+<script>
+import VHeader from "../components/Header";
+import VFooter from "../components/Footer";
+
+export default {
+  name: "Home",
+  components: {
+    VHeader,
+    VFooter, 
+    swiper,
+    swiperSlide
+  },
+  data() {
+    return {
+         swiperOption: {
+       // NotNextTick is a component's own property, and if notNextTick is set to true, the component will not instantiate the swiper through NextTick, which means you can get the swiper object the first time (if you need to use the get swiper object to do what Things, then this property must be true)
+       // notNextTick是一个组件自有属性，如果notNextTick设置为true，组件则不会通过NextTick来实例化swiper，也就意味着你可以在第一时间获取到swiper对象，假如你需要刚加载遍使用获取swiper对象来做什么事，那么这个属性一定要是true
+       notNextTick: true,
+       // swiper configs 所有的配置同swiper官方api配置
+       autoplay: 3000,
+       // direction : 'vertical',
+       effect:"coverflow",
+       grabCursor : true,
+       setWrapperSize :true,
+       // autoHeight: true,
+       // paginationType:"bullets",
+       pagination : '.swiper-pagination',
+       paginationClickable :true,
+       prevButton:'.swiper-button-prev',
+       nextButton:'.swiper-button-next',
+       // scrollbar:'.swiper-scrollbar',
+       //mousewheelControl : true,
+       observeParents:true,
+       // if you need use plugins in the swiper, you can config in here like this
+       // 如果自行设计了插件，那么插件的一些配置相关参数，也应该出现在这个对象中，如下debugger
+       // debugger: true,
+       // swiper callbacks
+       // swiper的各种回调函数也可以出现在这个对象中，和swiper官方一样
+       // onTransitionStart(swiper){
+       //   console.log(swiper)
+       // },
+       // more Swiper configs and callbacks...
+       // ...
+     },
+    };
+  }
+};
+</script>
+<style lang="less" scoped>
+
+.block-dark {
+  padding: 115px 206px;
+  background: #282b38;
+  display: flex;
+  .download-info {
+    display: flex;
+    padding-left: 128px;
+    dt {
+      font-size: 30px;
+      color: #ffffff;
+      line-height: 30px;
+      margin-bottom: 28px;
+    }
+    dd{
+      font-size: 14px;
+      color: #FFFFFF;
+      line-height: 30px;
+    }
+    .download-btn{
+      border:1px solid #fff;
+      opacity: .31;
+      display: inline-block;
+      margin-top: 30px;
+      width: 165px;
+      text-align: center;
+      height: 42px;
+      line-height: 42px;
+      border-radius: 2px;
+
+    }
+    .download-btn:last-of-type{
+      margin-left: 20px
+    }
+  }
+}
+</style>
