82b61d8766cb0cb708dc2e3026b631a85d5f1d91
woicon <hello@woicon.com>
2018-09-20 18:21:16 +0800

    fixed home vuex

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -95,7 +95,6 @@ const mutations = {
     })
     let indexData = [
       api.listByType({type: 1}), 
-      //banner列表
       api.list({pageNum: 1,pageSize: 10}), //公告列表
       api.classificationList({}), //交易对列表
       api.getTopTradeCoinPairs({}) //首页排行
@@ -104,17 +103,8 @@ const mutations = {
       .then(res => {
         state.banner = res[0].datas
         state.official = res[1].datas.list
-        state.category = res[2].datas
-        state.tradingCurrentIndex = res[2].datas[0].id
-        state.coinList = res[3].datas
+        state.coinList = res[2].datas
         return res
-      }).then(res => {
-        console.log(res)
-        api.getTradeInfoByZone({id: res[2].datas[0].id}).then(data => {
-          console.log(data)
-          state.tradingList = data.datas
-        })
-        
       }).catch(error => {
         console.log("error===>", error)
       })
